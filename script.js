ff7440d3d09f1c5b2ac368c4bf33caf175da774d
Gilberto André García Gaytán <84719490+gggandre@users.noreply.github.com>
2025-06-16 07:49:26 -0600

    Actualizar script.js

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ document.addEventListener('DOMContentLoaded', function () {
           <p>Te extraño muchísimo, a veces más de lo que puedo explicar. Siento un huequito en el pecho que solo tú puedes llenar, te amo.</p>
           <p>No dejo de pensar en ti, en tu voz, tu risa, tu forma de mirar. Me haces mucha falta, Montse.</p>
           <p>Ya quiero verte. Verte y abrazarte fuerte, quedarme ahí contigo sin preocuparme por nada más que por ese momento.</p>
-          <p>Ojalá podamos vernos pronto, ándale por favor. Yo estaré esperándote con todo mi amor y te daré muchos besos y abrazos porque te exytaño como no tienes idea .</p>
+          <p>Ojalá podamos vernos pronto, ándale por favor. Yo estaré esperándote con todo mi amor y te daré muchos besos y abrazos porque te extraño como no tienes idea .</p>
           <img src="assets/fotos/16.jpg" class="stitch-img" alt="Nosotros">
           <p class="detalle">Con todo mi amor, siempre tuyo 💖</p>
           <button class="cerrar-btn" onclick="window.close()">Cerrar 🌸</button>
